d1c999e9a81fb0e157e3a2dc6d8b6f0e3b978903
Anurag Soni <anurag@sonianurag.com>
2020-10-11 11:21:10 -0400

    remove the `mode` type

--- lwt_unix/postgres_lwt_unix.mli
+++ lwt_unix/postgres_lwt_unix.mli
@@ -1,15 +1,11 @@
 val src : Logs.Src.t
 
-type mode =
-  | Regular
-  | Tls of Tls.Config.client
-
 type destination =
   | Unix_domain of string
   | Inet of string * int
 
 val connect
-  :  ?mode:mode
+  :  ?tls_config:Tls.Config.client
   -> Postgres.Connection.User_info.t
   -> destination
   -> Postgres_lwt.t Lwt.t
