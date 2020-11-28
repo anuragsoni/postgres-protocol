d97c35dfc8e14e06924be14c612b2cb04e5426a5
Anurag Soni <anurag@sonianurag.com>
2020-11-27 21:17:44 -0500

    fix mirage example

--- example/mirage/unikernel.ml
+++ example/mirage/unikernel.ml
@@ -12,8 +12,8 @@ struct
     List.to_seq ids
     |> Seq.map (fun id ->
            let b = Bytes.create 4 in
-           Bytes.set_int32_be b 0 id;
-           Postgres.Frontend.Bind.make_param ~parameter:(Bytes.to_string b) `Binary ())
+           Caml.Bytes.set_int32_be b 0 id;
+           `Binary, Some (Bytes.to_string b))
     |> Array.of_seq
   ;;
 
