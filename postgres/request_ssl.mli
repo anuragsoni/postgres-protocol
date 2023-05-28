4a3320ec66619afed8dc33f47f80f2e56c48e59e
Anurag Soni <anurag@sonianurag.com>
2023-05-28 11:45:56 -0400

    Reset for 2023

--- postgres/request_ssl.mli
+++
@@ -1,6 +0,0 @@
-type t
-
-val create : ([ `Available | `Unavailable ] -> unit) -> t
-val next_operation : t -> [ `Write of bytes | `Read | `Fail of string | `Stop ]
-val report_write_result : t -> int -> unit
-val feed_char : t -> char -> unit
